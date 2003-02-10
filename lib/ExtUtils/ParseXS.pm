a148ada0b1a74d773ed05ae198038763e95a37aa
Ken Williams <kwilliams@cpan.org>
2003-02-10 04:16:29 +0000

    Explicitly untie and close the output file handle because ParseXS was holding the file handle open, preventing the compiler from opening it. [Randy W. Sims]
    Added an '--output FILENAME' flag to xsubpp and changed ParseXS to use
    the named file in the #line directives when the output file has an
    extension other than '.c' (i.e. '.cpp'). [Randy W. Sims]
    
    Added conditional definition of the PERL_UNUSED_VAR macro to the
    output file in case it's not already defined for backwards
    compatibility with pre-5.8 versions of perl. (Not sure if this is the
    best solution.) [Randy W. Sims]

--- lib/ExtUtils/ParseXS.pm
+++ lib/ExtUtils/ParseXS.pm
@@ -88,6 +88,7 @@ sub process_file {
   # other kind of reference, trust them that we can print to it.
   if (not ref $args{output}) {
     open my($fh), "> $args{output}" or die "Can't create $args{output}: $!";
+    $args{outfile} = $args{output};
     $args{output} = $fh;
   }
 
@@ -100,11 +101,15 @@ sub process_file {
   $pwd = cwd();
   
   if ($WantLineNumbers) {
-    my $cfile = $filename;
-    $cfile =~ s/\.xs$/.c/i or $cfile .= ".c";
+    my $cfile;
+    if ( $args{outfile} ) {
+      $cfile = $args{outfile};
+    } else {
+      $cfile = $filename;
+      $cfile =~ s/\.xs$/.c/i or $cfile .= ".c";
+    }
     tie(*PSEUDO_STDOUT, 'ExtUtils::ParseXS::CountLines', $cfile, $args{output});
     select PSEUDO_STDOUT;
-
   } else {
     select $args{output};
   }
@@ -254,6 +259,13 @@ EOM
     print $_;
   }
   die "Didn't find a 'MODULE ... PACKAGE ... PREFIX' line\n" unless defined $_;
+
+    print <<"EOF";
+#ifndef PERL_UNUSED_VAR
+#  define PERL_UNUSED_VAR(var) if (0) var = var
+#endif
+
+EOF
 
   print "$ExtUtils::ParseXS::CountLines::SECTION_END_MARKER\n" if $WantLineNumbers;
 
@@ -921,6 +933,8 @@ EOF
 
   chdir($orig_cwd);
   select($orig_fh);
+  untie *PSEUDO_STDOUT if tied *PSEUDO_STDOUT;
+
   return 1;
 }
 
@@ -1804,7 +1818,9 @@ sub DESTROY {
   print {$self->{fh}} $self->{buffer};
 }
 
-
+sub UNTIE {
+  # This sub does nothing, but is neccessary for references to be released.
+}
 
 
 
