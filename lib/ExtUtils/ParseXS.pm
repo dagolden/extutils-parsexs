2e002b18c83e75e5d3f7137ff96399b08b071feb
David Golden <dagolden@cpan.org>
2009-07-24 03:09:41 +0000

    Fixed "const char *" errors for 5.8.8 (and older) (RT#48104)

--- lib/ExtUtils/ParseXS.pm
+++ lib/ExtUtils/ParseXS.pm
@@ -976,10 +976,15 @@ EOF
 ##endif
 EOF
 
+  #Under 5.8.x and lower, newXS is declared in proto.h as expecting a non-const
+  #file name argument. If the wrong qualifier is used, it causes breakage with
+  #C++ compilers and warnings with recent gcc.
+  my $file_decl = ($] < 5.009) ? "char file[]" : "const char* file";
+
   #-Wall: if there is no $Full_func_name there are no xsubs in this .xs
   #so `file' is unused
   print Q(<<"EOF") if $Full_func_name;
-#    const char* file = __FILE__;
+#    $file_decl = __FILE__;
 EOF
 
   print Q("#\n");
