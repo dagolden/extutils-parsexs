e5d9bfb8a6d9ae1ec371917fc1044e8ba48c8be2
David Golden <dagolden@cpan.org>
2010-01-11 15:01:44 -0500

    bumped version to 2.22

--- lib/ExtUtils/ParseXS.pm
+++ lib/ExtUtils/ParseXS.pm
@@ -18,7 +18,7 @@ my(@XSStack);	# Stack of conditionals and INCLUDEs
 my($XSS_work_idx, $cpp_next_tmp);
 
 use vars qw($VERSION);
-$VERSION = '2.21_02';
+$VERSION = '2.22';
 $VERSION = eval $VERSION if $VERSION =~ /_/;
 
 use vars qw(%input_expr %output_expr $ProtoUsed @InitFileCode $FH $proto_re $Overload $errors $Fallback
